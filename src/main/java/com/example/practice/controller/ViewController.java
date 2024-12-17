08576f43ccb70165dabbf7c85e32a6529e200e6c
leetr <onevision93@naver.com>
2024-12-17 14:38:22 +0900

    1217

--- src/main/java/com/example/practice/controller/ViewController.java
+++ src/main/java/com/example/practice/controller/ViewController.java
@@ -16,18 +16,6 @@ public class ViewController {
 
     @GetMapping("/index")
     public String index(HttpSession session, Model model) {
-//        String memberId = (String) session.getAttribute("memberId");
-//        String memberPw = (String) session.getAttribute("memberPw");
-//        session.getAttribute("memberPw");
-//        if (session.getAttribute("memberId") == null) {
-//            return "loginMenu";
-//        } else {
-//            System.out.println("인덱스 아이디: " + "memberId");
-//            System.out.println("인덱스 비밀번호: " + "memberPw");
-//            model.addAttribute("memberId", memberId);
-//            model.addAttribute("memberPw", memberPw);
-//            return "index";
-//        }
         return "index";
     }
 
